f22310eb4d42bb8d7340b0c4401eb13ceec52bda
Iatin Andrei <62379702+Doominsky@users.noreply.github.com>
2022-05-30 11:35:23 +0300

    Basic enemy-player collision
    the player will die when in contact with an enemy. the current implementation is a prototype (hence no specific graphics are used). next version will include HP for both sides (see commented code)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,13 +11,18 @@ public class Player : MonoBehaviour
     private float rightEdge = 8.4f;
     private float leftEdge = -8.4f;
     private float movementX;
+    
+    public float playerHP;
 
+    //EnemyAI enemyAI;
+    //[SerializeField] GameObject enemy;
+  
     private Rigidbody2D playerBody;
 
     private void Awake()
     {
         playerBody = GetComponent<Rigidbody2D>();
-
+        //enemyAI = enemy.GetComponent<EnemyAI>();
     }
 
     void Update()
@@ -38,6 +43,25 @@ public class Player : MonoBehaviour
     private void MovePlayer()
     {
         playerBody.velocity = new Vector2(movementX * moveSpeed, scrollSpeed);
+    }
+
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        if(collision.gameObject.tag.Equals("Enemy")) 
+        {
+            /*playerHP -= enemyAI.enemyHP;
+            if(playerHP > enemyAI.enemyHP)
+                Destroy(enemyAI.gameObject);
+            if(playerHP == enemyAI.enemyHP)
+            {
+                Destroy(enemyAI.gameObject);
+                Destroy(this.gameObject);
+            }
+            if(playerHP < enemyAI.enemyHP)
+                Destroy(this.gameObject);*/
+            Destroy(this.gameObject);
+
+        }
     }
 
 }//class
